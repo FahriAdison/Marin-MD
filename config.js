94ed6996d40e776ba2c348700d71b06231216ca9
Yuta Togashi <92633211+FahriAdison@users.noreply.github.com>
2022-03-27 22:29:17 +0000

    Update config.js

--- config.js
+++ config.js
@@ -73,7 +73,7 @@ global.APIKeys = { // APIKey Here
   'https://pencarikode.xyz': 'pais', 
   'https://leyscoders-api.herokuapp.com': 'dappakntlll',
   'https://zekais-api.herokuapp.com': 'apikeymu',
-  'https://api.lolhuman.xyz': 'Deffbotz',
+  'https://api.lolhuman.xyz': 'ganti apikey mu :v',
 }
 //cari apikey sendiri ya gaes yak
 
